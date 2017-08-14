b2ec86ec85ec7ef0b0877780576da4cb8c5fcdd6
WoRuKu <wrk-berlin@web.de>
2017-08-14 12:32:31 +0200

    Set up a form for adding a new movie

--- app.rb
+++ app.rb
@@ -3,19 +3,24 @@ require 'movie'
 
 # Responds to GET requests for "/movies"
 get('/movies') do
-    # Set up an array of movies
-    @movies = []
-    # Set up new Movie objects
-    @movies[0] = Movie.new 
-    @movies[0].title = "Jaws"
-    @movies[1] = Movie.new 
-    @movies[1].title = "Alien "
-    @movies[2] = Movie.new 
-    @movies[2].title = "Terminator"
-    @movies[3] = Movie.new
-    @movies[3].title = "Startrek"
+  # Set up an array of movies
+  @movies = []
+  # Set up new Movie objects
+  @movies[0] = Movie.new 
+  @movies[0].title = "Jaws"
+  @movies[1] = Movie.new 
+  @movies[1].title = "Alien "
+  @movies[2] = Movie.new 
+  @movies[2].title = "Terminator"
+  @movies[3] = Movie.new
+  @movies[3].title = "Startrek"
 
-    # Load "views/index.erb"
-    erb :index
+  # Load "views/index.erb"
+  erb :index
+end
+
+get ('/movies/new') do
+  # Load "views/new.erb" 
+  erb :new
 end
 
